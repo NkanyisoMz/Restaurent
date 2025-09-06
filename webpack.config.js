ca199c52bc7c486258cc2c789dd111d7ed06e15d
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-09-06 20:41:46 +0200

    Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        loader: 'file-loader',
+      },
     ],
   },
   plugins: [
